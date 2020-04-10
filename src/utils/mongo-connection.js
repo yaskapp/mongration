bd1fdf43610b50ada88af69f7cbdf6909786e3e6
sideral <a31415926535@gmail.com>
2020-04-09 20:12:12 -0500

    Return db instead of client

--- src/utils/mongo-connection.js
+++ src/utils/mongo-connection.js
@@ -20,7 +20,10 @@ function MongoConnection(config){
 }
 
 MongoConnection.prototype.connect = function(cb){
-    MongoClient.connect(this.connectionUri || this.getConnectionUri(), cb);
+    MongoClient.connect(this.connectionUri || this.getConnectionUri(), (err, client) => {
+        if(err) return cb(err);
+        cb(null, client.db(this.db));
+    });
 }
 
 MongoConnection.prototype.getConnectionUri = function(){
